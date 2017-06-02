b2526f423bd59e3a71a48a8616cf7cc2ac01af56
Conor <cjhfitz@gmail.com>
2017-06-02 12:16:51 -0400

    added def from_string() method

--- Object-Oriented Python/inheritance_and_advanced_objects/morse.py
+++ Object-Oriented Python/inheritance_and_advanced_objects/morse.py
@@ -14,11 +14,23 @@ class Letter:
     def __iter__(self):
         yield from self.pattern # similar to self.slots from inventory
 
+    @classmethod # provide an alternate constructor
+    def from_string(cls, input_string):
+        split_input = input_string.split('-')
+        new_pattern = []
+        for blip in split_input:
+            if blip == 'dot':
+                new_pattern.append('.')
+            else:
+                new_pattern.append('_')
+        return cls(new_pattern)
+
+
 
 class S(Letter):
     def __init__(self):
         pattern = ['.', '.', '.']
         super().__init__(pattern)
 
-my_pattern = Letter(['.', '_', '.'])
+my_pattern = Letter.from_string('dot-dot')
 print(str(my_pattern))
