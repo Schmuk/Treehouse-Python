a469d87ee607ecb7f6e52dfde6a4d75fc4ba0a03
Conor <cjhfitz@gmail.com>
2017-06-03 17:14:44 -0400

    Python Practice

--- yatzy/dice.py
+++ yatzy/dice.py
@@ -4,13 +4,19 @@ import random
 class Die:
     def __init__(self, sides=2, value=0):
         if not sides >= 2:
-            raise ValueError("Must have at least 2 sides")
+            raise ValueError('Must have at least 2 sides')
         if not isinstance(sides, int):
-            raise ValueError("Sides must be a whole number")
+            raise ValueError('Sides must be a whole number')
+        if not value >= 0:
+            raise ValueError('Value must be at least 0')
+        if not isinstance(value, int):
+            raise ValueError('Value must be a whole number')
+
+        self.sides = sides
         self.value = value or random.randint(1, sides)
 
     def __int__(self):
-        return self.value
+        return int(self.value)
 
     def __eq__(self, other):
         return int(self) == other
@@ -25,10 +31,10 @@ class Die:
         return int(self) < other
 
     def __ge__(self, other):
-        return int(self) > other or int(self) == other
+        return int(self) >= other or int(self) == other
 
     def __le__(self, other):
-        return int(self) < other or int(self) == other
+        return int(self) <= other or int(self) == other
 
     def __add__(self, other):
         return int(self) + other
